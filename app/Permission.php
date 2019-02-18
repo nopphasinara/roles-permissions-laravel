1437b9ff6e81512faddbb12a1eaee5f522ba3e11
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-02-18 21:23:50 +0700

    Update source

---
+++ app/Permission.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use \Spatie\Permission\Models\Permission as BasePermission;
+
+class Permission extends BasePermission
+{
+    public static function defaultPermissions()
+    {
+        return [
+            'view_users',
+            'add_users',
+            'edit_users',
+            'delete_users',
+
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'delete_roles',
+
+            'view_posts',
+            'add_posts',
+            'edit_posts',
+            'delete_posts',
+
+            'view_agents',
+            'add_agents',
+            'edit_agents',
+            'delete_agents',
+        ];
+    }
+}
